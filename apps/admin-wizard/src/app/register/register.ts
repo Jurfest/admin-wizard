b7de985c086ee203457bed0f81658ae7052e84a1
Jurfest <jurfest.apps@gmail.com>
2025-09-19 15:23:25 -0300

    feat: improve navbar and logo layout

--- apps/admin-wizard/src/app/register/register.ts
+++ apps/admin-wizard/src/app/register/register.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { InputComponent, Button, Logo } from '@admin-wizard/ui-design-system';
+import { InputComponent, Button } from '@admin-wizard/ui-design-system';
 
 @Component({
   selector: 'app-register',
-  imports: [InputComponent, Button, Logo],
+  imports: [InputComponent, Button],
   templateUrl: './register.html',
   styleUrl: './register.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
